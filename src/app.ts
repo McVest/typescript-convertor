9a0ec23c55365efe9bfdd6d9f4a852a1fccc2c9a
msgda <pixman28@gmail.com>
2023-08-30 00:11:38 +0300

    Creating a PromptService, PromptType

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
-export class App{
-    run(){
-        console.log('Done')
+import { PromptService } from './core/prompt/prompt.service';
+
+
+export class App {
+    async run() {
+        const res = await new PromptService().input<number>('Число', 'number');
+        console.log(res);
     }
 }
 const app = new App();
-app.run()
+app.run();
